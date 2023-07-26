f772491dbc28c9342fa033b7f00df51754a78305
Creata21 <yerezhepabdulla@gmail.com>
2023-07-27 01:07:02 +0600

    create models

--- routes/routes.go
+++ routes/routes.go
@@ -6,9 +6,9 @@ import(
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("/user/signup", controllers.SignUp())
+	incomingRoutes.POST("/user/signup", controllers.Signup())
 	incomingRoutes.POST("/user/login", controllers.Login())
-	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewAdmin())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
 	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
 	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery( ))
 }
